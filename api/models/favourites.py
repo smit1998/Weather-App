9f524598a0e5595cdc44280d0a85cc886c89f515
smit98 <smitdobaria@gmail.com>
2021-10-07 16:35:44 +1100

    api

---
+++ api/models/favourites.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.conf import settings
+from api.models.userProfile import UserProfile
+
+class Favourites(models.Model):
+    class Meta:
+        db_table = "favourites"
+    
+    id = models.AutoField(primary_key=True)
+    location = models.CharField(max_length=40)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='User_id', on_delete=models.CASCADE)
