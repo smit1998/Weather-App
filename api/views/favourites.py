4ad9790e6b9822294f11e22b97b67652e6afbcb5
smit98 <smitdobaria@gmail.com>
2021-10-07 17:22:09 +1100

    back-end-api views

---
+++ api/views/favourites.py
@@ -0,0 +1,44 @@
+from django.shortcuts import get_list_or_404, get_object_or_404
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
+from rest_framework import status
+from django.contrib.auth.models import User
+from api.models.favourites import Favourites
+from api.serializer.favourites import AddFavouriteLocationSerializer
+
+
+class AddFavouriteLocation(APIView):
+    permission_classes = (IsAuthenticated,)
+
+def post(self, request):
+        serializer = AddFavouriteLocationSerializer(data=request.data)
+
+        if serializer.is_valid():
+            data = request.data
+            user = request.user
+            
+            if checkIfAlreadyInFavourite(user, data):
+                return Response({"message": "Already added to favourites"}, status=status.HTTP_201_CREATED)
+            
+            self.addToFavourites(user, data)
+            return Response({"message": "Location added to favourites"}, status=status.HTTP_201_CREATED)
+                
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def addToFavourites(self, user, data):
+        if not Favourites.objects.filter(location=data['location'], user=user).exists():
+            new_location = Favourites(
+                location=data.get('location'),
+                user=user
+            )
+            new_location.save()
+    
+def checkIfAlreadyInFavourite(user, data):
+    check_location = Favourites.objects.filter(location=data.get('location'), user=user).exists()
+    if user_location:
+        return True
+    return False
+
+
